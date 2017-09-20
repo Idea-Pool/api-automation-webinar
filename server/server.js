2be7c69db7ba127c16539b5d9e727d0431443d36
Laszlo, Szikszai <Szikszai.Laszlo@wolterskluwer.com>
2017-09-20 18:19:58 +0200

    Fixed exit codes

--- server/server.js
+++ server/server.js
@@ -8,18 +8,28 @@ const args = require('yargs')
     }).argv;
 
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const app = jsonServer.create();
 const router = jsonServer.router('server/data.json');
 const middlewares = jsonServer.defaults();
 
 router.render = (req, res) => {
     res.jsonp({
         data: res.locals.data
-    })
+    });
 };
 
-server.use(middlewares);
-server.use('/api', router);
-server.listen(args.port, () => {
+app.use(middlewares);
+app.use('/api', router);
+const server = app.listen(args.port, () => {
     console.log(`Server is running on ${args.port} port...`);
 });
+
+const exit = () => {
+    server.close(() => {
+        process.exit(0);
+    });
+};
+process.on('SIGINT', exit);
+process.on('SIGTERM', exit);
+process.on('exit', exit);
+process.on('uncaughtException', exit);
